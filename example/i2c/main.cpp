1e38b15cb82c11b0aa5a06830c44aad1e2490be6
murathepeyiler <murathepeyiler@gmail.com>
2022-01-28 22:57:35 +0300

    Read and write function tested.

--- example/i2c/main.cpp
+++ example/i2c/main.cpp
@@ -2,6 +2,12 @@
 #include "logger.h"
 
 int main() {
-    I2CDev dev{"/dev/i2c-1"};
-    dev.send8(0x23, 0x20, "test", 5);
+    I2CDev dev{"/dev/i2c-0"};
+    dev.send8(0x23, 0x20, "test\n", 6);
+    auto res = dev.read8(0x23, 0x20, 5);
+    if (res == nullptr) {
+        LOG_FATAL("Read failed");
+    } else {
+        LOG_INFO("Received message %s", *res);
+    }
 }
