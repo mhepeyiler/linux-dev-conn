a62dcfa9a08911ba35faab0b11cbcc698bc2f160
murathepeyiler <murathepeyiler@gmail.com>
2022-02-09 21:44:12 +0300

    Doxygen was added to interface classes.

--- src/include/comm_device.h
+++ src/include/comm_device.h
@@ -3,11 +3,53 @@
 
 #include <memory>
 
+
+/**
+ * @brief Abstract class for connection devices. 
+ * 
+ * @details All connection classes, such as i2c, spi, must be inherited from here.
+ */
 class CommDevice {
 public:
-    virtual void send(uint16_t devAddress, uint16_t address, const char* data, std::size_t dataSize) noexcept = 0;
-    virtual void read(uint16_t devAddress, uint16_t address, std::size_t dataSize, char* data) noexcept = 0;
-    virtual std::unique_ptr<char> read(uint16_t devAddress, uint16_t address, std::size_t dataSize) noexcept = 0;
+
+    /**
+     * @brief Generic send method over data bus.
+     * 
+     * @param devAddress Device address to send data
+     * @param address Register address
+     * @param data Data to be send
+     * @param dataSize Size of data
+     * 
+     * @return Returns true if the send successful
+     */
+    virtual bool send(uint16_t devAddress, uint16_t address, const uint8_t* data, std::size_t dataSize) noexcept = 0;
+
+    /**
+     * @brief Generic read method over data bus.
+     * 
+     * @details This function uses user given preallocated space to write data.
+     * 
+     * @param devAddress Device address to read data
+     * @param address Register address
+     * @param dataSize Data to be read
+     * @param data Preallocated memory space to write data. If read data exceeds allocated memory space, it is undefined behaviour.
+     * 
+     * @return Returns true if the read is successful
+     */
+    virtual bool read(uint16_t devAddress, uint16_t address, std::size_t dataSize, uint8_t* data) noexcept = 0;
+
+    /**
+     * @brief Generic read method over data bus.
+     * 
+     * @details This function uses dynamic memory allocation to transfer data to user. 
+     * 
+     * @param devAddress Device address to read data
+     * @param address Register address
+     * @param dataSize Data to be read
+     * 
+     * @return If the read fails then it returns nullptr, otherwise data pointer.
+     */
+    virtual std::unique_ptr<uint8_t> read(uint16_t devAddress, uint16_t address, std::size_t dataSize) noexcept = 0;
 };
 
 #endif
--- src/include/io_device.h
+++ src/include/io_device.h
@@ -1,10 +1,26 @@
 #ifndef LINUX_DEV_CONN_SRC_INCLUDE_IO_DEVICE_H
 #define LINUX_DEV_CONN_SRC_INCLUDE_IO_DEVICE_H
 
+/**
+ * @brief Interface class for digital I/O functionalities.
+ * 
+ */
 class IODevice {
 public:
-    virtual void open() = 0;
-    virtual void close() = 0;
+
+    /**
+     * @brief Opens the pin
+     * 
+     * @return true if the operation is succesful
+     */
+    virtual bool open() = 0;
+    
+    /**
+     * @brief Closes the pin
+     * 
+     * @return true if the operation is succesful.
+     */
+    virtual bool close() = 0;
 };
 
 #endif
